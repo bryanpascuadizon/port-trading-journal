39a9156e4c340fb1fa41c994b7207a96b04dc4f8
Bryan Dizon <bryanpascuadizon@gmail.com>
2025-07-24 08:52:11 +0800

    feat: added error message when confirm password field is empty

--- lib/validations/auth-schema.ts
+++ lib/validations/auth-schema.ts
@@ -11,7 +11,9 @@ export const signUpSchema = z
       .min(5, "Name must be at least 5 characters")
       .max(20, "Name must be at most 20 characters"),
     password: z.string().min(5, "Password must be at least 5 characters"),
-    confirmPassword: z.string().min(5).optional(),
+    confirmPassword: z
+      .string()
+      .min(5, "Confirm password must be at least 5 characters"),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Confirm password should be the same with your password",
