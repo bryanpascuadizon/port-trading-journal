481c71c9c47547c724bf77da5d8a59b4444a39aa
Bryan Dizon <bryanpascuadizon@gmail.com>
2025-08-16 16:49:39 +0800

    feat: added loading state when logging out

--- components/SidePanel/SidePanelLogoutButton.tsx
+++ components/SidePanel/SidePanelLogoutButton.tsx
@@ -1,18 +1,27 @@
 "use client";
 
 import { signOutUser } from "@/lib/actions/auth-actions";
-import { Power } from "lucide-react";
+import { LoaderCircle, Power } from "lucide-react";
+import { useTransition } from "react";
 
 const SidePanelLogoutButton = () => {
-  const handleLogout = async () => {
-    await signOutUser();
+  const [isLogoutPending, startLogoutTransition] = useTransition();
+  const handleLogout = () => {
+    startLogoutTransition(async () => {
+      await signOutUser();
+    });
   };
   return (
     <div
       className="flex gap-2 items-center cursor-pointer"
       onClick={handleLogout}
     >
-      <Power className="h-5" />
+      {isLogoutPending ? (
+        <LoaderCircle className="animate-spin" />
+      ) : (
+        <Power className="h-5" />
+      )}
+
       <p>Logout</p>
     </div>
   );
